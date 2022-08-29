db87337cf381181cf723d4dacbc013c425026959
gutenLEE <3bnm1128@naver.com>
2022-08-29 23:09:00 +0900

    feat(스트라이크 판별) - 완료

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -3,6 +3,7 @@ package baseball;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author gutenlee
@@ -26,7 +27,22 @@ public class BaseballTest {
         assertThat(answer.length()).isEqualTo(3);
     }
 
+    @Test
+    void returnStrike_test() throws Exception {
+        String mockAnswer = "352";
+        String tryAnswer = "153";
+        assertTrue(isStrike(mockAnswer, tryAnswer));
+    }
 
+    private boolean isStrike(String mockAnswer, String tryAnswer) {
+        for (int i = 0; i < 3; i++) {
+            char answerChar = mockAnswer.charAt(i);
+            char tryChar = tryAnswer.charAt(i);
+            if (answerChar == tryChar)
+                return true;
+        }
+        return false;
+    }
 
 
 }
