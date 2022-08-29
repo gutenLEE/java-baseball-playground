1a276dc695b878444cf2ce2b1111de74e4b746bb
gutenLEE <3bnm1128@naver.com>
2022-08-29 23:16:07 +0900

    feat(볼 판별) - 완료

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -3,6 +3,7 @@ package baseball;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -31,6 +32,17 @@ public class BaseballTest {
         return false;
     }
 
+    private boolean isBall(String mockAnswer, String tryAnswer) {
+        for (int i = 0; i < 3; i++) {
+            char answerChar = mockAnswer.charAt(i);
+            char tryChar = tryAnswer.charAt(i);
+            String aLetter = tryAnswer.substring(i, i + 1);
+            if (answerChar != tryChar && mockAnswer.contains(aLetter))
+                return true;
+        }
+        return false;
+    }
+
     @Test
     void createAnswerResult_should_be_3_letters() throws Exception {
         String answer = createAnswer();
@@ -42,7 +54,24 @@ public class BaseballTest {
         String mockAnswer = "352";
         String tryAnswer = "153";
         assertTrue(isStrike(mockAnswer, tryAnswer));
+
+        mockAnswer = "123";
+        tryAnswer = "456";
+        assertFalse(isStrike(mockAnswer, tryAnswer));
     }
+
+    @Test
+    void returnBall_test() throws Exception {
+        String mockAnswer = "123";
+        String tryAnswer = "367";
+        assertTrue(isBall(mockAnswer, tryAnswer));
+
+        // 예외 케이스 - 스트라이크인 경우
+        mockAnswer = "123";
+        tryAnswer = "523";
+        assertFalse(isBall(mockAnswer, tryAnswer));
+    }
+
 
 
 
